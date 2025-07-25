58a6a44740a88a8d7792298b530d20ef993f6b90
Kusuma2004 <135133937+Kusuma2004@users.noreply.github.com>
2025-07-26 00:05:26 +0530

    Update loanApi.js

--- frontend/src/api/loanApi.js
+++ frontend/src/api/loanApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/v1' });
+const API = axios.create({ baseURL: 'https://loan-application-kk-1.onrender.com/api/v1' });
 
 export const createLoan = (data) => API.post('/loans', data);
 export const makePayment = (loanId, data) => API.post(`/loans/${loanId}/payments`, data);
